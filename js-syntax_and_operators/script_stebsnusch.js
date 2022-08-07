e45375e6f629e617ac3dc3a427681d60a7e5a397
Taresu <tsalata@alunos.utfpr.edu.br>
2022-08-07 16:28:28 -0300

    Add the the best script form ever for this practice

---
+++ js-syntax_and_operators/script_stebsnusch.js
@@ -0,0 +1,40 @@
+function compareNumbers(num1, num2) {
+    const firstString = createFirstString(num1, num2);
+    const lastString = createLastString(num1, num2);
+
+    return `${firstString} ${lastString}`;
+}
+
+function createFirstString(num1, num2) {
+    let isEqual = '';
+
+    if (num1 !== num2) { isEqual = "não" };
+
+    return `Os numeros ${num1} e ${num2} ${isEqual} são iguais.`
+}
+
+function createLastString(num1, num2) {
+    const sum = num1 + num2;
+    const compare10 = sum > 10;
+    const compare20 = sum > 20;
+
+    let result10 = 'menor';
+    let result20 = 'menor';
+
+    if (compare10) { result10 = 'maior' };
+    if (compare20) { result20 = 'maior' };
+
+    return `Sua soma é ${sum}, que é ${result10} que 10 e ${result20} que 20.\n`;
+}
+
+//Exemplos de combinações condicionais com números diferentes
+console.log(compareNumbers(5, 10));
+console.log(compareNumbers(3, 5));
+console.log(compareNumbers(20, 10));
+console.log(compareNumbers(17, 3));
+
+//Exemplos de combinações condicionais com números iguais
+console.log(compareNumbers(8, 8));
+console.log(compareNumbers(4, 4));
+console.log(compareNumbers(15, 15));
+console.log(compareNumbers(10, 10));
